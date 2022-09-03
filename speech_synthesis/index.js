53659487713f9f35c3409a62694d847cd0fc5cf7
tomit4 <mosssap@gmail.com>
2022-09-03 01:11:38 -0700

    :pencil2: Continuing tot est speech_synthesis

--- speech_synthesis/index.js
+++ speech_synthesis/index.js
@@ -13,14 +13,16 @@ setTimeout(() => {
 }, 1000)
 
 function populateVoices() {
-    voices = this.getVoices() // returns an empty array unfortunately...
+    voices = this.getVoices() // returns an empty array with Google API, try from Windows VM
     voicesDropdown.innerHTML = voices
         .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>}`)
         .join('')
 }
 
 function setVoice() {
-   console.log('Changing voice')
+   // console.log('Changing voice')
+   // console.log(this.value)
+    msg.voice = voices.find(voice => voice.name === this.value)
 }
 speechSynthesis.addEventListener('voiceschanged', populateVoices)
 voicesDropdown.addEventListener('change', setVoice)
